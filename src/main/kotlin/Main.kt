cd0e1411c74c6b056356dfc2358540204f394bee
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-11-15 14:26:00 +0100

    feat!: update todos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,24 @@
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
-import com.github.kotlintelegrambot.dispatcher.*
-import com.github.kotlintelegrambot.entities.ChatId
-import com.github.kotlintelegrambot.entities.ParseMode
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.logging.LogLevel
 
 fun main(args: Array<String>) {
-
     val bot = bot {
         token = "<Your Token>"
         timeout = 60
         logLevel = LogLevel.Network.Body
 
         dispatch {
-            command("<Your Command>"){
-                // TODO
+            command("<Your Command>") {
+                TODO("Add here what the bot should do")
             }
 
             text("<Text>") {
-                // TODO
+                TODO("Add here what the bot should do")
             }
         }
     }
-
     bot.startPolling()
 }
